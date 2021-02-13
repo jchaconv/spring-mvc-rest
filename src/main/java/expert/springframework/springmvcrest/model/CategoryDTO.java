1954014dd8401c9c7c84bd747720c7d25bb29b07
jchaconv <jcaaron.chacon@outlook.com>
2021-02-13 10:45:48 -0500

    added some tests

--- src/main/java/expert/springframework/springmvcrest/model/CategoryDTO.java
+++ src/main/java/expert/springframework/springmvcrest/model/CategoryDTO.java
@@ -1,6 +1,10 @@
 package expert.springframework.springmvcrest.model;
 
+import lombok.Data;
+
+@Data
 public class CategoryDTO {
+
     private Long id;
     private String name;
 }
---
+++ src/test/java/expert/springframework/springmvcrest/mapper/VendorMapperTest.java
@@ -0,0 +1,40 @@
+package expert.springframework.springmvcrest.mapper;
+
+import expert.springframework.springmvcrest.domain.Vendor;
+import expert.springframework.springmvcrest.model.VendorDTO;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class VendorMapperTest {
+
+    public static final String NAME = "someName";
+
+    VendorMapper vendorMapper = VendorMapper.INSTANCE;
+
+    @Test
+    public void vendorToVendorDTO() throws Exception {
+        //given
+        Vendor vendor = new Vendor();
+        vendor.setName(NAME);
+
+        //when
+        VendorDTO vendorDTO = vendorMapper.vendorToVendorDTO(vendor);
+
+        //then
+        assertEquals(vendor.getName(), vendorDTO.getName());
+    }
+
+    @Test
+    public void vendorDTOtoVendor() throws Exception {
+        //given
+        VendorDTO vendorDTO = new VendorDTO();
+        vendorDTO.setName(NAME);
+
+        //when
+        Vendor vendor = vendorMapper.vendorDTOtoVendor(vendorDTO);
+
+        //then
+        assertEquals(vendorDTO.getName(), vendor.getName());
+    }
+}
